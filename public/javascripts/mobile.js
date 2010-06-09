ea83f2ea3b7d2164665d5adaf14581618e603084
D Sofer <d@sofer.com>
2010-06-09 13:12:00 +0100

    files added

---
+++ public/javascripts/mobile.js
@@ -0,0 +1,12 @@
+var jQT = new $.jQTouch({
+  icon:'apple-touch-icon.png',
+  //addGlossToIcon: false,
+  startupScreen:"apple-touch-startup.png",
+  statusBar:'black-translucent',
+  preloadImages:[
+    '/stylesheets/img/backButton.png',
+    '/stylesheets/img/grayButton.png',
+    '/stylesheets/img/whiteButton.png',
+    '/stylesheets/img/loading.gif'
+  ]
+});
